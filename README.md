dc4924ee9453420f40f48815e6aebb79e587cb31
Bilalzafar0020 <bilalafridi0020@gmail.com>
2023-05-22 17:21:02 +0500

    Having dark-mode + buttons Voice

--- README.md
+++
@@ -1,6 +0,0 @@
-# Calculator-having-dark-mode-and-button-voice
-ScreenShot overview => 
-![Screenshot (16)](https://github.com/Bilalzafar0020/Calculator-having-dark-mode-and-button-voice/assets/115065049/2b2fb020-c6e8-41de-9e65-888e816b636b)
-
-![Screenshot (14)](https://github.com/Bilalzafar0020/Calculator-having-dark-mode-and-button-voice/assets/115065049/24657638-9ff4-4cb3-bf1b-1e7e1273f8ad)
-
--- script.js
+++ script.js
@@ -21,8 +21,8 @@ function audio1(){
 
 // it is the method but it is not stoping the user to not type more than i want
 
-function value_Display(value) {
-	typing.innerText += value; 
+function value_Display(value) { //  second method is to add in onclick in html like that 
+	typing.innerText += value;  // id of dispaly area + .innerText += "1" ;  display.innerText += "1"
 }
 
 //   second method to restrict the user from not typing more than 26 but css property is best
@@ -51,10 +51,19 @@ try{
 		result.innerText = "Error";
 	}
 
+	//  to cut more then 15 numbers in result so it should'nt overflow
 		if (result.innerText.length > 12) {
 		 result.innerText = result.innerText.slice(0, 15);  //  only 15 numbers will display 
 		};
-	
+
+		let replaced = typing.innerText;
+
+// if (typing.innerText.include(' / ') ||  typing.innerText.include('*') ){
+// 	replaced = typing.innerText.replaceAll('x',"*").replaceAll('&#247;','/')
+// 	typing.innerText= eval(replaced)
+// }
+// typing.innerText= eval(replaced)
+
 }
 
 
@@ -70,6 +79,9 @@ function clearResult() {
 function remove(){
 	typing.innerText = typing.innerText.slice(0,-1);
 }
+
+
+
 
 
 //   dark mode setting
